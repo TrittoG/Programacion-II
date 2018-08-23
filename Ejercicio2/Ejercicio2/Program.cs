be9d1021cc128d8f89c84494cfe5dcaedbd7b659
TrittoG <36174147+TrittoG@users.noreply.github.com>
2018-08-22 22:16:41 -0300

    Add files via upload

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ejercicio 2";
+            int numero;
+            double cubo;
+            string numeroString;
+
+            Console.WriteLine("ingrese un numero");
+            numeroString = Console.ReadLine();
+            numero = int.Parse(numeroString);
+
+            if(numero < 0)
+            {
+                Console.WriteLine("error numero menos que 0");
+            }
+
+            Console.WriteLine("numero elevado a la 2: " +Math.Pow(numero, 2));
+            cubo = Math.Pow(numero, 3);
+
+            Console.WriteLine("el numero elevado a la 3: " + cubo);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ejercicio 3/ejercicio 3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ejerccio 3";
+            //Console.BackgroundColor = (ConsoleColor)(12);
+
+            int numero,i,j,contador;
+            string numeroString;
+
+            Console.WriteLine("ingrese un numero");
+            numeroString = Console.ReadLine();
+
+            numero = int.Parse(numeroString);
+            
+            for(i=0;i<=numero;i++)
+            {
+                contador = 0;
+                for (j=1;j<=i;j++)
+                {
+                    if(i%j == 0)
+                    {
+                        contador++;                  
+                    }
+
+                }
+                if(contador == 2)
+                {
+                    Console.WriteLine("{0, 10}",i);
+                }
+            }
+            
+
+            Console.ReadKey();
+            Console.Clear();
+            Console.ReadKey();
+        }
+    }
+}
