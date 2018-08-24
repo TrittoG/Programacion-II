f68c0f10cd7bde9668066d783f73c2b749d4091d
TrittoG <36174147+TrittoG@users.noreply.github.com>
2018-08-23 22:12:27 -0300

    Add files via upload

---
+++ ejercicio12/ejercicio12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ejercicio 12";
+
+            int numero, suma = 0;
+            string ahre;
+            char respuesta = 's';
+
+            do
+            {
+                Console.WriteLine("valor: ");
+                ahre = Console.ReadLine();
+
+                if(int.TryParse(ahre, out numero))
+                {
+                    suma = numero + suma;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+
+                Console.WriteLine("sumando {0}", suma);
+                Console.WriteLine("quiere ingresar otro valor? s/n ");
+                ahre = Console.ReadLine();
+                respuesta = char.Parse(ahre);
+
+            } while (ValidarRespuesta.ValidaS_N(respuesta));
+        }
+    }
+}
---
+++ ejercicio13/ejercicio13/Conversor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio13
+{
+    class Conversor
+    {
+        public static string DecimalBinario(double doble)
+        {
+            int cociente;
+
+            while(cociente != 1)
+            {
+
+            }
+            return "asd";
+        }
+
+        public static double BinarioDecimal(string binario)
+        {
+            return 1;
+        }
+    }
+}
