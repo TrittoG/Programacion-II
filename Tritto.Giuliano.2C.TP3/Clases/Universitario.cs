b4b3c6886c95157bad55a87d5227bd0d3927f976
TrittoG <ggtgiuli@hotmail.com>
2018-11-01 17:05:55 -0300

    tp

---
+++ Tritto.Giuliano.2C.TP3/Clases/Universitario.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAbstractas
+{
+    public abstract class Universitario : Persona
+    {
+        private int legajo;
+
+        public Universitario()
+        {
+
+        }
+
+        public Universitario(int legajo, string nombre, string apellido, string dni, ENacionalidad nacionalidad) : base(nombre,apellido,nacionalidad,dni)
+        {
+            this.legajo = legajo;
+        }
+
+
+        protected virtual string MostrarDatos()
+        {
+            StringBuilder st = new StringBuilder();
+
+            st.AppendLine(base.ToString());
+            st.AppendLine("Legajo: " + this.legajo);
+
+            return st.ToString();
+        }
+
+        protected abstract string ParticiparEnClase();  
+
+        public static bool operator ==(Universitario pg1, Universitario pg2)
+        {
+            if(pg1.GetType() == pg2.GetType() && (pg1.legajo == pg2.legajo || pg1.DNI == pg2.DNI))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Universitario pg1, Universitario pg2)
+        {
+            return !(pg1 == pg2);
+        }
+
+         
+    }
+}
