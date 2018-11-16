6d8f4dcf53bafb51083a312d38e0bc2889028e90
alumno <alumno>
2018-11-15 21:11:59 -0300

    practica parcial

---
+++ Practica Parcial/20180628-SP - Alumno/Entidades/Votacion.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Entidades
+{
+    public class Votacion
+    {
+        public enum EVoto { Afirmativo, Negativo, Abstencion, Esperando }
+
+        //delegado y evento
+        public delegate void Voto(string str, EVoto eVoto);
+        public event Voto EventoVotoEfectuado;
+
+
+        private string nombreLey;
+        private Dictionary<string, EVoto> senadores;
+
+        private short contadorAfirmativo;
+        private short contadorNegativo;
+        private short contadorAbstencion;
+
+        public Votacion()
+        {
+
+        }
+
+    
+
+        public string NombreLey
+        {
+            get
+            {
+                return this.nombreLey;
+            }
+            set
+            {
+                this.nombreLey = value;
+            }
+        }
+
+        public short ContadorAbstencion
+        {
+            get
+            {
+                return this.contadorAbstencion;
+            }
+            set
+            {
+                this.contadorAbstencion = value;
+            }
+        }
+
+        public short ContadorNegativo
+        {
+            get
+            {
+                return this.contadorNegativo;
+            }
+            set
+            {
+                this.contadorNegativo = value;
+            }
+        }
+
+        public short ContadorAfirmativo
+        {
+            get
+            {
+                return this.contadorAfirmativo;
+            }
+            set
+            {
+                this.contadorAfirmativo = value;
+            }
+        }
+
+
+        public Votacion(string nombreLey, Dictionary<string, EVoto> senadores)
+        {
+            this.nombreLey = nombreLey;
+            this.senadores = senadores;
+        }
+
+        public void Simular()
+        {
+            // Reseteo contadores
+            this.contadorAbstencion = 0;
+            this.contadorAfirmativo = 0;
+            this.contadorNegativo = 0;
+            // Itero todos los Senadores
+            for (int index = 0; index < this.senadores.Count; index++)
+            {
+                // Duermo el hilo
+                System.Threading.Thread.Sleep(134);
+
+                // Leo el senador actual
+                KeyValuePair<string, EVoto> k = this.senadores.ElementAt(index);
+                // Generador de número aleatorio
+                Random r = new Random(k.Key.ToString().Length + DateTime.Now.Millisecond);
+                // Modifico el voto de forma aleatoria
+                this.senadores[k.Key] = (EVoto)r.Next(0, 3);
+
+                // Invocar Evento
+                EventoVotoEfectuado.Invoke(k.Key, this.senadores[k.Key]);
+
+                // Incrementar contadores
+                if(this.senadores[k.Key] == EVoto.Abstencion)
+                {
+                    this.ContadorAbstencion++;
+                }
+                else if(this.senadores[k.Key] == EVoto.Afirmativo)
+                {
+                    this.ContadorAfirmativo++;
+                }
+                else if(this.senadores[k.Key] == EVoto.Negativo)
+                {
+                    this.ContadorNegativo++;
+                }
+
+            }
+        }
+
+       
+
+
+
+        
+
+    }
+}
---
+++ Practica Parcial/20180628-SP - Alumno/TestUnitario/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+
+namespace TestUnitario
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        int contador = 0;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            try
+            {
+                Votacion vot = new Votacion();
+
+                SerializarXML<Votacion> ser = new SerializarXML<Votacion>();
+
+                ser.Guardar(null, vot); //null para que entre al catch
+            }
+            catch (Exception e)
+            {
+
+                Assert.IsInstanceOfType(e, typeof(ErrorArchivoException));
+            }
+        }
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            System.Collections.Generic.Dictionary<string, Votacion.EVoto> senadores;
+            senadores = new System.Collections.Generic.Dictionary<string, Votacion.EVoto>();
+
+            senadores.Add("NICOLAS", Votacion.EVoto.Negativo);
+            senadores.Add("NICOLAO", Votacion.EVoto.Negativo);
+            senadores.Add("Giuliano", Votacion.EVoto.Afirmativo);
+            Votacion v = new Votacion("nombre", senadores);
+
+            //agrego al evento el contador para ver cuantas veces 
+            v.EventoVotoEfectuado += Contador;
+
+            v.Simular();
+
+            //si la cantidad de veces que usa el evento es igual a los senadores
+            Assert.AreEqual(senadores.Count, contador);
+        }
+
+
+        /// <summary>
+        /// Contador para ver cuantas veces se llama al evento
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="eVoto"></param>
+        public void Contador(string str, Votacion.EVoto eVoto)
+        {
+            contador++;
+        }
+
+    }
+}
